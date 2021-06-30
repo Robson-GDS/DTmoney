3b3e56ccb985daa2c9bcd07fdf4a93005c618d93
Robson Gomes <robson.080490@gmail.com>
2021-06-30 08:11:41 -0300

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Iniciando projeto</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
