894ede33efa1450866fd9fa7f675f969aefc0488
Robson Gomes <robson.080490@gmail.com>
2021-07-05 08:26:07 -0300

    add functionality to the button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react'
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,6 +12,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal 
     isOpen={isOpen} 
@@ -39,19 +42,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       />
 
       <TransactionTypeContainer>
-        <button
+        <RadioBox
           type="button"
+          onClick={() => { setType('deposit'); }}
+          isActive={type === 'deposit'}
         >
           <img src={incomeImg} alt="Entrada" />
           <span>Entrada</span>
-        </button>
+        </RadioBox>
 
-        <button
+        <RadioBox
           type="button"
+          onClick={() => { setType('withdraw'); }}
+          isActive={type === 'withdraw'}
         >
           <img src={outcomeImg} alt="Saida" />
           <span>Saida</span>
-        </button>
+        </RadioBox>
       </TransactionTypeContainer>
 
       <input 
