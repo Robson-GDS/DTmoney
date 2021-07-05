9543e1b2a0484fc623bc219a5cdd898dc0021b7d
Robson Gomes <robson.080490@gmail.com>
2021-07-05 07:50:16 -0300

    creating type buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
-import { Container } from './styles'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+
+import { Container, TransactionTypeContainer } from './styles'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -34,6 +37,22 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         type="number"
         placeholder="Valor"
       />
+
+      <TransactionTypeContainer>
+        <button
+          type="button"
+        >
+          <img src={incomeImg} alt="Entrada" />
+          <span>Entrada</span>
+        </button>
+
+        <button
+          type="button"
+        >
+          <img src={outcomeImg} alt="Saida" />
+          <span>Saida</span>
+        </button>
+      </TransactionTypeContainer>
 
       <input 
         placeholder="Categoria"
