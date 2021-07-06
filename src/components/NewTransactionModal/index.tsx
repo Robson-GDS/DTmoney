9226e1d35a3a45279221db412552ac1faa85e04c
Robson Gomes <robson.080490@gmail.com>
2021-07-06 09:45:57 -0300

    formatting values

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../services/api'
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
@@ -20,12 +21,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = ({
       title,
       value,
       category,
       type,
-    })
+    });
+
+    api.post('/transactions', data)
   }
 
   return (
